28dda19b6238c6c4f2b8157833d3c23a8b45c798
el_primo <vladislav1kuznetsov@gmail.com>
2024-08-03 00:09:00 +0300

    *

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -15,7 +15,7 @@ export function TextArea({ className, ...props }: Props) {
         className="w-full resize-none rounded-[0.75rem] border-2 border-[#80d0b3] px-4 py-1 text-[#000000] placeholder:text-[#777777]"
         {...props}
       />
-      <Button className="absolute right-[3rem] border-none p-0 active:bg-transparent">
+      <Button className="absolute right-[2rem] border-none p-0 active:bg-transparent">
         <img className="size-8" src="/images/sendIcon.png" />
       </Button>
     </>
